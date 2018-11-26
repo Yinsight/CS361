459a768acedba552573dfe4c52cde607ab190393
Krutika Wadhwa <41032373+krutikaw@users.noreply.github.com>
2018-11-26 16:38:47 -0500

    Update ScannerDemo.java

--- Scanner-Parser/ScannerDemo.java
+++ Scanner-Parser/ScannerDemo.java
@@ -1,10 +1,10 @@
 /**
- * @author Christelle
+ * @author Krutika Wadhwa
  * 
  */
 public class ScannerDemo {
 
-	private static String file1 = "C:\\Users\\cscharff\\Desktop\\TESTSCANNERPARSER2010\\GIVEN\\PARSER\\prog2.jay";
+	private static String file1 = "C:\\\\Users\\\\Krutika Wadhwa\\\\Desktop\\\\ParserScannerTests(1) (1)\\\\ParserScannerTests\\\\SCANNER\\\\prog1.jay";
     //need to change the filepath
 	private static int counter = 1;
 
@@ -17,7 +17,8 @@ public class ScannerDemo {
 		while (!ts.isEndofFile()) {
 			// TO BE COMPLETEDs
             // How to get token here?
-            System.out.println("Token " + counter + ":" + ts.nextToken().toString());
+            System.out.println("Token " + counter + ":" + ts.nextToken().toString()+"\n");
+            
             // how to write token1 token2 token3?
             counter = counter + 1;
             
