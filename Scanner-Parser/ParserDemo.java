b981ccb59b58d1b6c9db6e67b69223e7472b8a41
Yunting Yin <heatheryin1213@gmail.com>
2018-12-03 00:46:40 -0500

    Update ParserDemo.java

--- Scanner-Parser/ParserDemo.java
+++ Scanner-Parser/ParserDemo.java
@@ -1,7 +1,9 @@
 public class ParserDemo {
 
+	private static String file1 = "/Users/heatheryin/Desktop/test/prog4.jay";
+	
 	public static void main(String[] args) {
-		TokenStream tStream = new TokenStream(args[0]);
+		TokenStream tStream = new TokenStream(file1);
 		System.out.println("test1");
 		ConcreteSyntax cSyntax = new ConcreteSyntax(tStream);
 		System.out.println("test2");
